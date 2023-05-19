1bb7c565f161266c87d06f3a2e2ca13793c83abc
Rafaelnotty <113991887+rafaelnotty@users.noreply.github.com>
2023-05-19 22:58:22 +0000

    Mensaje del commit

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.requests import Request
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.exception_handler(HTTPException)
-async def http_exception_handler(request, exc):
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: StarletteHTTPException):
     if exc.status_code == 404:
         return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
-    return exc
+    return await default_exception_handler(request, exc)
+
+@app.exception_handler(Exception)
+async def default_exception_handler(request: Request, exc: Exception):
+    return templates.TemplateResponse("excepciones.html", {"request": request, "exc": exc}, status_code=500)
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
